21c39001e10294629e96358ff7851e298131d12a
rbarazi <rbarazi@gmail.com>
2008-11-09 01:51:02 +0400

    Adding Categories, posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :comments
+
+  map.resources :posts
+
+  map.resources :categories
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
