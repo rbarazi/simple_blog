5454dd3a9de19242976f82c59d0de2a3348728b8
rbarazi <rbarazi@gmail.com>
2008-11-30 20:51:03 +0400

    Adding English language and stylesheet

--- config/environment.rb
+++ config/environment.rb
@@ -24,5 +24,5 @@ Rails::Initializer.run do |config|
   # The internationalization framework can be changed to have another default locale (standard is :en) or more load paths.
   # All files from config/locales/*.rb,yml are added automatically.
   # config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'my', 'locales', '*.{rb,yml}')]
-  config.i18n.default_locale = :ar
+  config.i18n.default_locale = :en
 end
